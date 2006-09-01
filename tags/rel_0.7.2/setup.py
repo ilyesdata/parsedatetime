57e9ba6206798fe9f93d0b7dea1682909018a610
bear42 <bear42@669c90c8-9b19-0410-a54c-b59d71393f99>
2006-09-01 07:13:15 +0000

    Tagging trunk as 0.7.2

---
+++ tags/rel_0.7.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+
+use_setuptools()
+
+import setuptools
+
+desc='Parse human-readable date/time expressions',
+
+setuptools.setup(name='parsedatetime',
+      version='0.7.2',
+      description=desc,
+      author='Mike Taylor',
+      author_email='bear@code-bear.com',
+      url='http://code-bear.com/code/parsedatetime/',
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      packages=['parsedatetime'],
+      platforms=['Any'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Library',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Text Processing',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                  ]
+     )
+
